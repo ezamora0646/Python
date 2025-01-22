66921e9a297f6b5426dc5ab7670f40c3a8620d59
Edwin Zamora <dieciseis@Edwins-MBP.lan>
2025-01-21 22:21:54 -0800

    notes from day 1 lecture videos, upskilllist python course

---
+++ Basics.py
@@ -0,0 +1,16 @@
+# Python variables
+#same as vectors in R
+#strings (chars), floaters (decimals), integers (whole)
+
+string = "Hello World"
+print(string)
+#even though we have assigned string, we still need to use print
+
+string2 = print("Hello World")
+string2 # this approach does not store the string contents bc print doesn't return any values
+
+num1= 66
+print(string+str(num1)) #str() coerces data type in python
+
+
+
